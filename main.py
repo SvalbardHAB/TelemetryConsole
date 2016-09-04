aa7048972911c34d00224aac71e2c86168836e39
William Eustace <weustace@eustacesheen.me.uk>
2016-09-04 09:33:35 +0200

    Updated bitshifting etc.

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import serial,sys,time,os
 
 #const_default_serial_port = "/dev/tty.usbserial-A6009CSG"
 #const_default_serial_port = "/dev/tty.usbserial-A6009CSG"
-const_default_serial_port = "/dev/cu.usbmodem1556311"
+const_default_serial_port = "/dev/cu.usbmodem1657141"
 const_default_serial_baud = 115200
 const_data_bad_time = 3.0
 const_serial_timeout = 0.01
 const_lng_mult = 1
 
-EFM_boards = [0]
-cloud_boards = [0,1]
-hygro_boards = [0]
+EFM_boards = []
+cloud_boards = [2]
+hygro_boards = [2]
 
 def nameLogFile(base):
   file_counter = 0
@@ -52,10 +52,9 @@ def printData():
     print("Altitude: {0} m".format(latest[7]))
     print("P: {0} hPa  |  T: {1} °C  |  Lat,long: {2}, {3}".format(latest[6],latest[8],latest[9],latest[10]))
     print("Photodiode counts: {0} | Average level: {1}".format(latest[11],latest[12]))
-    if latest[3] in EFM_boards:
-        print("EFM min: {0}  |  EFM max: {1}".format(latest[13],latest[14]))
+
     if latest[3] in cloud_boards:
-        print("Cloud average: {0}/1024  |  SD: {1}".format(latest[15],latest[16]))
+        print("Cloud density: {0}".format(latest[15]))
     if latest[3] in hygro_boards:
         print("Relative humidity: {0}%  |  Outside temp: {1} °C".format(latest[17],latest[18]))
     print("GPS {0}; has {1}got more than or three satellites; hdop {2} less than 20m.".format("valid" if last_status[3]  else "invalid","" if last_status[2]  else "not ", "is" if last_status[1]  else "is not"))
@@ -72,9 +71,9 @@ def processdata(received):
     proclog = 0 #open(logfile_names[1],'a')
     split = received.split(",")
     try:
-        latest[0]=int(split[0],16) #CRC pass/fail
-        latest[1] = int(split[1],16) #RSSI
-        latest[2] = int(split[2],16) #SNR
+        latest[0]=int(split[0]) #CRC pass/fail
+        latest[1] = int(split[1]) #RSSI
+        latest[2] = int(split[2]) #SNR
         latest[3] = int(split[3],16) #device ID
         proclog = open(logfile_names[latest[3]+1],'a')
         latest[4] = int(split[4],16) #status byte
@@ -93,21 +92,19 @@ def processdata(received):
 
         latest[11] = int(split[19],16)#photodiode count
         latest[12] = int(split[20],16)#photodiode avg level
-        if latest[3] in EFM_boards:
-            latest[13] = int(split[21],16) #EFM byte 1
-            latest[14] = int(split[22],16) #EFM byte 2
-        else:
-            latest[13] = 0
-            latest[14] = 0
+
+        latest[13] = 0 #ex efm bytes—RIP EFM...
+        latest[14] = 0
         if latest[3] in cloud_boards:
-            latest[15] = (int(split[23],16)<<8 | int(split[24],16))    #cloud detector avg
-            latest[16] = int(split[25],16) #cloud detector standard deviation
+            latest[15] = (int(split[21],16)<<8 | int(split[22],16))  -512  #cloud detector average
+
+            latest[16] = 0 #now overlooked.
         else:
             latest[15] = 0
             latest[16] = 0
         if latest[3] in hygro_boards:
-            latest[17] = int(split[26],16) #relative humidity
-            latest[18] = int(split[27],16) #external temperature
+            latest[17] = int(split[23],16) #relative humidity
+            latest[18] = int(split[24],16) -128 #external temperature in excess 128 format
         else:
             latest[17] = 0
             latest[18] = 0
